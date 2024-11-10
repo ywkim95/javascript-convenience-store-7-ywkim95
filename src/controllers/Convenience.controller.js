c60d3a81853c81a9fa0e7cfbb98c62c75acdf9e8
KimYoungWoo <rladuddn3924@gmail.com>
2024-11-11 06:11:23 +0900

    FEAT: 편의점 컨트롤러 및 Validate 정의

--- src/controllers/Convenience.controller.js
+++ src/controllers/Convenience.controller.js
@@ -1,27 +1,244 @@
-import Product from '../models/Product.model.js';
-import Promotion from '../models/Promotion.model.js';
-import readFile from '../utils/read-file.js';
+import { DateTimes } from '@woowacourse/mission-utils';
+import { productFileName, promotionFileName, readFile } from '../utils/file-io.js';
+import io from '../utils/io.js';
 import ProductController from './Product.controller.js';
 import PromotionController from './Promotion.controller.js';
+import OutputView from '../views/Output.view.js';
+import InputView from '../views/Input.view.js';
+import Receipt from '../models/Receipt.model.js';
+import ReceiptController from './Receipt.controller.js';
+import Validate from '../utils/Validate.js';
 
 export default class ConvenienceController {
   #productController;
   #promotionController;
 
-  start() {
-    const rawProducts = this.readFiles('products.md');
-    const products = rawProducts.map((product) => new Product(...product));
-    this.#productController = new ProductController(products);
+  constructor() {
+    const rawProducts = this.readFiles(productFileName);
+    this.#productController = new ProductController(rawProducts);
 
-    const rawPromotions = this.readFiles('promotions.md');
-    const promotions = rawPromotions.map(
-      (promotion) => new Promotion(...promotion)
-    );
-    this.#promotionController = new PromotionController(promotions);
+    const rawPromotions = this.readFiles(promotionFileName);
+    this.#promotionController = new PromotionController(rawPromotions);
+  }
+
+  async start() {
+    while(true) {
+      try {
+        const products = await this.inputProducts();
+        await this.buyProducts(products);
+      } catch (e) {
+        io.printOutput(e.message);
+        continue;
+      }
+      const again = await InputView.purchaseAgain();
+      if (again.toUpperCase() === 'N') {
+        break;
+      }
+    }
   }
 
   readFiles(fileName) {
     const file = readFile(fileName);
-    return file.split('\n').map((line) => line.split(','));
+    const lines = file.split('\n');
+    lines.shift();
+    lines.pop();
+    return lines.map((line) => line.split(','));
   }
+
+  async inputProducts() {
+    OutputView.printWelcome();
+    this.#productController.printProductList();
+
+    const rawInput = await InputView.readProducts();
+    return rawInput.split(',').map((product) => {
+      const cleanProduct = product.replace(/[\[\]]/g, '');
+      return cleanProduct.split('-');
+    });
+  }
+
+  async buyProducts(products) {
+    const receipts = new ReceiptController();
+
+    for (const productNameQuantity of products) {
+      const receiptProduct = await this.processProduct(productNameQuantity);
+      const receipt = new Receipt(receiptProduct);
+      receipts.push(receipt);
+    }
+    const isMembership = await InputView.membership();
+    this.printReceipt(receipts, isMembership);
+    this.updateStockQuantities(receipts);
+    this.#productController.updateFile();
+  }
+
+  async processProduct(productNameQuantity) {
+    let { productName, quantity } = this.getProductNameQuantity(
+      productNameQuantity
+    );
+    const { promotionProduct, nonPromotionProduct } =
+      this.getPromotionProduct(productName);
+
+    let promotionQuantity = 0;
+    let nonPromotionQuantity = 0;
+    let totalQuantity = this.getTotalQuantity(
+      promotionProduct,
+      nonPromotionProduct
+    );
+
+    Validate.validateQuantity(totalQuantity, quantity);
+
+    const hasPromotion = this.checkPromotion(promotionProduct);
+    if (hasPromotion) {
+      const result = await this.applyPromotion(promotionProduct, quantity);
+      quantity = result.quantity;
+      promotionQuantity = result.promotionQuantity;
+      nonPromotionQuantity = result.nonPromotionQuantity;
+    } else {
+      nonPromotionQuantity = quantity;
+      Validate.validateQuantity(nonPromotionProduct.quantity, nonPromotionQuantity);
+    }
+
+    let name = promotionProduct ? promotionProduct.name : nonPromotionProduct.name;
+    let price = promotionProduct
+      ? promotionProduct.price
+      : nonPromotionProduct.price;
+
+    return {
+      name,
+      quantity,
+      price,
+      promotionQuantity,
+      nonPromotionQuantity,
+    };
+  }
+
+  async applyPromotion(promotionProduct, quantity) {
+    let promotionQuantity = 0;
+    let nonPromotionQuantity = 0;
+    const promotion = this.#promotionController.findPromotion(
+      promotionProduct.promotion
+    );
+
+    if (promotionProduct.quantity >= quantity) {
+      const result = await this.handleSufficientPromotionStock(
+        promotionProduct,
+        quantity,
+        promotion
+      );
+      quantity = result.quantity;
+      promotionQuantity = result.promotionQuantity;
+    } else {
+      const result = await this.handleInsufficientPromotionStock(
+        promotionProduct,
+        quantity,
+        promotion
+      );
+      quantity = result.quantity;
+      promotionQuantity = result.promotionQuantity;
+      nonPromotionQuantity = result.nonPromotionQuantity;
+    }
+
+    return { quantity, promotionQuantity, nonPromotionQuantity };
+  }
+
+  async handleSufficientPromotionStock(promotionProduct, quantity, promotion) {
+    let promotionQuantity = 0;
+    if (quantity % (promotion.buy + promotion.free) === 0) {
+      promotionQuantity = quantity / (promotion.buy + promotion.free);
+    } else {
+      promotionQuantity = Math.floor(
+        quantity / (promotion.buy + promotion.free)
+      );
+      if (
+        quantity % (promotion.buy + promotion.free) === promotion.buy
+      ) {
+        const input = await InputView.addPromotion(promotionProduct.name);
+        if (input.toUpperCase() === 'Y') {
+          quantity += 1;
+          promotionQuantity += 1;
+        }
+      }
+    }
+    return { quantity, promotionQuantity };
+  }
+
+  async handleInsufficientPromotionStock(promotionProduct, quantity, promotion) {
+    const promotionSum = promotion.buy + promotion.free;
+    let promotionQuantity = Math.floor(promotionProduct.quantity / promotionSum);
+    const additionalQuantity = quantity - promotionQuantity * promotionSum;
+    let nonPromotionQuantity = 0;
+    const input = await InputView.nonPromotion(promotionProduct.name, additionalQuantity);
+
+    if (input.toUpperCase() === 'Y') {
+      nonPromotionQuantity = additionalQuantity;
+    } else {
+      OutputView.printSubNonPromotion(promotionProduct.name);
+      quantity -= additionalQuantity;
+    }
+
+    return { quantity, promotionQuantity, nonPromotionQuantity };
+  }
+
+  printReceipt(receipts, isMembership) {
+    receipts.printProducts();
+    receipts.printPromotion();
+    OutputView.printReceiptDivider();
+    receipts.printPrice(isMembership);
+  }
+
+  updateStockQuantities(receipts) {
+    for (const receipt of receipts.value) {
+      const promotionQuantity = receipt.quantity - receipt.nonPromotionQuantity;
+      const nonPromotionQuantity = receipt.nonPromotionQuantity;
+      const promotionProduct = this.#productController.findPromotionProduct(receipt.name);
+      const nonPromotionProduct = this.#productController.findNonPromotionProduct(receipt.name);
+      if (promotionProduct) {
+        promotionProduct.updateQuantity(promotionProduct.quantity - promotionQuantity);
+      }
+      if (nonPromotionProduct) {
+        nonPromotionProduct.updateQuantity(nonPromotionProduct.quantity - nonPromotionQuantity);
+      }
+    }
+  }
+
+  getProductNameQuantity(productNameQuantity) {
+    const [productName, quantityStr] = productNameQuantity;
+    const quantity = parseInt(quantityStr, 10);
+    return { productName, quantity };
+  }
+
+  getPromotionProduct(productName) {
+    const promotionProduct =
+      this.#productController.findPromotionProduct(productName);
+    const nonPromotionProduct =
+      this.#productController.findNonPromotionProduct(productName);
+    Validate.validatePromotionProduct(promotionProduct, nonPromotionProduct);
+    return { promotionProduct, nonPromotionProduct };
+  }
+
+
+  getTotalQuantity(promotionProduct, nonPromotionProduct) {
+    let totalQuantity = 0;
+    if (promotionProduct) {
+      totalQuantity += promotionProduct.quantity;
+    }
+    if (nonPromotionProduct) {
+      totalQuantity += nonPromotionProduct.quantity;
+    }
+    return totalQuantity;
+  }
+
+  checkPromotion(promotionProduct) {
+    if (promotionProduct) {
+      const promotion = this.#promotionController.findPromotion(
+        promotionProduct.promotion
+      );
+      return promotion.includePromotion(
+        promotionProduct.promotion,
+        DateTimes.now()
+      );
+    }
+    return false;
+  }
+
+
 }
--- src/utils/Validate.js
+++ src/utils/Validate.js
@@ -1,1 +1,72 @@
-class Validate {}
+import ERROR_MESSAGE from './error-message.js';
+
+class Validate {
+  static validateYN(input) {
+    const upperInput = input.toUpperCase();
+    if (upperInput !== 'Y' && upperInput !== 'N') {
+      throw new Error(ERROR_MESSAGE.INVALID_YN);
+    }
+  }
+
+  static validateQuantity(totalQuantity, quantity) {
+    if (totalQuantity < quantity) {
+      throw new Error(
+        ERROR_MESSAGE.OVERFLOW_QUANTITY
+      );
+    }
+  }
+
+  static validatePromotionProduct(promotionProduct, nonPromotionProduct) {
+    if (!promotionProduct && !nonPromotionProduct) {
+      throw new Error(ERROR_MESSAGE.NO_PRODUCT);
+    }
+  }
+
+  static validatePromotion(promotion) {
+    if (!promotion) {
+      throw new Error(ERROR_MESSAGE.PROMOTION_NOT_FOUND);
+    }
+  }
+
+  static validateProductInput(input) {
+    if (!input || input.trim() === '') {
+      throw new Error(ERROR_MESSAGE.NO_INPUT);
+    }
+    this.validateProducts(input);
+  }
+
+  static validateProducts(input) {
+    const products = input.split(',');
+    for (const product of products) {
+      this.validateProduct(product);
+    }
+  }
+
+  static validateProduct(product) {
+    const parts = product.split('-');
+    this.validateDash(parts);
+    let [productName, quantityStr] = parts;
+    this.validateProductName(productName);
+    productName = productName.trim();
+    const quantity = parseInt(quantityStr, 10);
+  }
+
+  static validateDash(parts) {
+    if (parts.length !== 2) {
+      throw new Error(ERROR_MESSAGE.DASH);
+    }
+  }
+
+  static validateProductName(productName) {
+    if (!productName || productName.trim() === '') {
+      throw new Error(ERROR_MESSAGE.PRODUCT_NAME_NOT_FOUND);
+    }
+  }
+
+  static validateProductQuantity(quantity) {
+    if (isNaN(quantity) || quantity < 1) {
+      throw new Error(ERROR_MESSAGE.INVALID_QUANTITY);
+    }
+  }
+}
+export default Validate;
