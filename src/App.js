c74a85046bf2fb08531d06beeb78f9585b96bafe
KimYoungWoo <rladuddn3924@gmail.com>
2024-11-11 06:11:59 +0900

    FEAT: App run에 편의점 컨트롤러 선언 및 사용

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
+import ConvenienceController from './controllers/Convenience.controller.js';
+
 class App {
-  async run() {}
+  async run() {
+    const convenienceController = new ConvenienceController();
+    await convenienceController.start();
+  }
 }
 
 export default App;
